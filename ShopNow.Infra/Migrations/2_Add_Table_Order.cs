e714d5d70a0a3626b85c13a4a8bd2156354f3cce
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-09-15 23:00:03 -0300

    feat: Add tanle order and edit table item

---
+++ ShopNow.Infra/Migrations/2_Add_Table_Order.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace ShopNow.Infra.Migrations
+{
+    [Migration(2)]
+    public class AddTableOrder : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("tb_order")
+                .WithColumn("cd_order").AsInt32().PrimaryKey().Identity()
+                .WithColumn("code").AsString(200).NotNullable()
+                .WithColumn("cpf").AsString(11).NotNullable()
+                .WithColumn("issue_date").AsDateTime().NotNullable()
+                .WithColumn("freight").AsDecimal(18, 2).NotNullable()
+                .WithColumn("sequence").AsInt32().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("tb_order");
+        }
+    }
+}
