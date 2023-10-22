a5a8ad13f7700e69afc6e3c9b0510173759b161e
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-10-22 14:47:53 -0300

    feat: Add controller method for SimulateFreight use case

---
+++ ShopNow.IntegrationTests/Specs/Controllers/SimulateFreightControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using ShopNow.Domain.Entities;
+using ShopNow.Dtos;
+using ShopNow.IntegrationTests.Setup;
+using ShopNow.Tests.Shared.Extensions;
+using System.Net;
+
+namespace ShopNow.IntegrationTests.Specs.Controllers
+{
+    public class SimulateFreightControllerTests : ApiBase
+    {
+        private const string URL_BASE = "/freights";
+
+        [Test]
+        public async Task GetFreightShouldBeAbleToReturnFreight()
+        {
+            var items = new List<Item>
+            {
+                new Item(1, "Guitarra", "Eletrônicos", 1000, 100, 50, 15, 3),
+                new Item(2, "Amplificador", "Eletrônicos", 5000, 50, 50, 50, 22),
+                new Item(3, "Cabo", "Eletrônicos", 30, 10, 10, 10, 1),
+            };
+            _context.AddRange(items);
+            await _context.SaveChangesAsync();
+
+            var simulateFreightInput = new SimulateFreightInput
+            {
+                OrderItems = new List<OrderItemInput>
+                {
+                    new OrderItemInput()
+                    {
+                        IdItem = 1,
+                        Count = 1,
+                    },
+                    new OrderItemInput()
+                    {
+                        IdItem = 2,
+                        Count =  1,
+                    },
+                    new OrderItemInput()
+                    {
+                        IdItem = 3,
+                        Count = 3,
+                    }
+                }
+            };
+
+            var response = await _httpClient.PostAsync(URL_BASE, simulateFreightInput.ToJsonContent());
+            var responseAsJson = await response.Content.ReadAsStringAsync();
+            var expectedFreight = 280M.Serialize();
+
+            using(new AssertionScope())
+            {
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+                responseAsJson.ShouldBeAnEquivalentJson(expectedFreight);
+            }
+        }
+    }
+}
---
+++ ShopNow/Controllers/FreightsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopNow.Domain.Repositories;
+using ShopNow.Dtos;
+using ShopNow.UseCases;
+
+namespace ShopNow.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FreightsController : ControllerBase
+    {
+        private readonly IItemRepository _itemRepository;
+
+        public FreightsController(IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(SimulateFreightInput simulateFreightInput)
+        {
+            var simulateFreight = new SimulateFreight(_itemRepository);
+            var freight = await simulateFreight.Execute(simulateFreightInput);
+
+            return Ok(freight);
+        }
+    }
+}
