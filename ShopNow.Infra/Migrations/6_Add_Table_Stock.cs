3b730dbd23b70e2a250fa9d4aa52fdccf86eb760
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-11-21 20:14:35 -0300

    feat: Add table stock entry

---
+++ ShopNow.Infra/Migrations/6_Add_Table_Stock.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace ShopNow.Infra.Migrations
+{
+    [Migration(6)]
+    public class AddTableStock : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("tb_stock_entry")
+                .WithColumn("cd_stock").AsInt32().Identity().PrimaryKey()
+                .WithColumn("id_item").AsInt32().ForeignKey()
+                .WithColumn("operation").AsString(20).NotNullable()
+                .WithColumn("quantity").AsInt32().NotNullable()
+                .WithColumn("date").AsInt32().NotNullable();
+
+            Create.ForeignKey("FK_Stock_Item")
+                .FromTable("tb_stock_entry").ForeignColumn("id_item")
+                .ToTable("tb_item").PrimaryColumn("cd_item");
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Stock_Item");
+            Delete.Table("tb_stock_entry");
+        }
+    }
+}
