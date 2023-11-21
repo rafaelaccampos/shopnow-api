e41b2eeacca1496f30235538116dfe2bd9d8cb34
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-11-21 20:37:20 -0300

    feat: Add column status to order

---
+++ ShopNow.Infra/Migrations/7_Add_Table_Column_Status_In_Table_Order.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace ShopNow.Infra.Migrations
+{
+    public class AddTableColumnStatusInTableOrder : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("tb_order")
+                .AddColumn("status").AsString(2);
+        }
+
+        public override void Down()
+        {
+            Delete.Column("status");
+        }
+    }
+}
