1220c0572a93f5ac9545958628fb6dc0bf1796e3
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-12-05 23:30:43 -0300

    ref: Create an item builder for test ShouldBeAbleToCalculateFreightWhenFreightIsHigherThanTen

--- ShopNow.UnitTests/Specs/Entities/FreightCalculatorTests.cs
+++ ShopNow.UnitTests/Specs/Entities/FreightCalculatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using ShopNow.Domain.Checkout.Entities;
 using ShopNow.Domain.Checkout.Services;
+using ShopNow.Tests.Shared.Builders;
 
 namespace ShopNow.UnitTests.Specs.Entities
 {
@@ -19,7 +20,18 @@ namespace ShopNow.UnitTests.Specs.Entities
         [Test]
         public void ShouldBeAbleToCalculateFreightWhenFreightIsHigherThanTen()
         {
-            var item = new Item(2, "Amplificador", "Eletrônicos", 5000, 50, 50, 50, 22);
+            var item = new ItemBuilder()
+                    .WithId(1)
+                    .WithDescription("Amplificador")
+                    .WithCategory("Eletrônicos")
+                    .WithPrice(5000)
+                    .WithWidth(50)
+                    .WithHeight(50)
+                    .WithLength(50)
+                    .WithWeight(22)
+                    .Generate();
+
+            //var item = new Item(2, "Amplificador", "Eletrônicos", 5000, 50, 50, 50, 22);
 
             var freight = FreightCalculator.Calculate(item);
 
