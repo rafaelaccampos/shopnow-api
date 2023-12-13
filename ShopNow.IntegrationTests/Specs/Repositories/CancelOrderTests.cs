ec7ec12138dd57d0104c3128a7d40acf40cf915b
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-12-12 23:01:56 -0300

    fix: Fix test ShouldBeAbleToCancelOrder

--- ShopNow.IntegrationTests/Specs/Repositories/CancelOrderTests.cs
+++ ShopNow.IntegrationTests/Specs/Repositories/CancelOrderTests.cs
@@ -15,7 +15,6 @@ namespace ShopNow.IntegrationTests.Specs.Repositories
         [Test]
         public async Task ShouldBeAbleToCancelAnOrder()
         {
-            //Arrange
             var item = new Item(1, "Guitarra", "Eletrônicos", 1000, 100, 50, 15, 3);
             _context.Add(item);
             await _context.SaveChangesAsync();
@@ -24,13 +23,12 @@ namespace ShopNow.IntegrationTests.Specs.Repositories
             var issueDate = DateTime.Now;
             var order = new Order(cpf, issueDate, 1);
             order.AddItem(item, 1);
+            _context.Add(order);
             await _context.SaveChangesAsync();
 
-            //Act
             var cancelOrder = GetService<CancelOrder>();
-            cancelOrder?.Execute(order.Code);
+            await cancelOrder.Execute(order.Code);
 
-            //Assert
             var orderRepository = GetService<IOrderRepository>();
             var orderCanceled = await orderRepository.Get(order.Code);
             orderCanceled!.Status.Should().Be("Cancelled");
