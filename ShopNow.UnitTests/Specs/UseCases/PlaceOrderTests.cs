08b59bd73dabef6f25e8ce3b91d654480ed09330
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-12-17 07:51:27 -0300

    feat: Add test for check if event placed order and order is saved when place order

---
+++ ShopNow.UnitTests/Specs/UseCases/PlaceOrderTests.cs
@@ -0,0 +1,85 @@
+﻿using Bogus.Extensions.Brazil;
+using FluentAssertions.Execution;
+using NSubstitute;
+using ShopNow.Controllers;
+using ShopNow.Domain.Checkout.Entities;
+using ShopNow.Domain.Checkout.Factory;
+using ShopNow.Domain.Checkout.Repositories;
+using ShopNow.Domain.Shared.Event;
+using ShopNow.Dtos;
+using ShopNow.Infra.Shared.Event;
+using ShopNow.UnitTests.Setup;
+using ShopNow.UseCases;
+
+namespace ShopNow.UnitTests.Specs.UseCases
+{
+    public class PlaceOrderTests : BaseTest
+    {
+        private IItemRepository _itemRepository;
+        private IOrderRepository _orderRepository;
+        private ICouponRepository _couponRepository;
+        private IAbstractRepositoryFactory _abstractRepositoryFactory;
+        private EventBus _eventBus;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _itemRepository = Substitute.For<IItemRepository>();
+            _orderRepository = Substitute.For<IOrderRepository>();
+            _couponRepository = Substitute.For<ICouponRepository>();
+            _abstractRepositoryFactory = Substitute.For<IAbstractRepositoryFactory>();
+            _eventBus = Substitute.For<EventBus>();
+
+            _abstractRepositoryFactory.CreateItemRepository().Returns(_itemRepository);
+            _abstractRepositoryFactory.CreateOrderRepository().Returns(_orderRepository);
+            _abstractRepositoryFactory.CreateCouponRepository().Returns(_couponRepository);
+        }
+
+        [Test]
+        public async Task ShouldBeAbleToCallSaveOrderAndEventPlaceOrderWhenOrderIsPlaced()
+        {
+            var placeOrderInput = new PlaceOrderInput
+            {
+                Cpf = Faker.Person.Cpf(false),
+                OrderItems = new List<OrderItemInput>
+                {
+                    new OrderItemInput
+                    {
+                        IdItem = 1,
+                        Count = 1,
+                    },
+                },
+                IssueDate = DateTime.Now,
+                Coupon = "VALE20"
+            };
+
+            var item = new Item(1, "Guitarra", "Eletrônicos", 1000, 100, 10, 15, 3);
+            var coupon = new Coupon("VALE20", 20);
+            var order = new Order(placeOrderInput.Cpf, placeOrderInput.IssueDate);
+            order.AddItem(item, 1);
+
+            _orderRepository.Count().Returns(0);
+            _itemRepository.FindByIdAsync(placeOrderInput.OrderItems.First().IdItem).Returns(item);
+            _couponRepository.FindByCode(placeOrderInput.Coupon).Returns(coupon);
+            await _orderRepository.Save(order);
+
+            //Act
+            var placeOrder = new PlaceOrder(_abstractRepositoryFactory, _eventBus);
+            var placeOrderOutput = await placeOrder.Execute(placeOrderInput);
+
+            var orderItemsDto = order.OrderItems.Select(o =>
+                new ItemDto
+                {
+                    Id = placeOrderInput.OrderItems.First().IdItem,
+                    Count = placeOrderInput.OrderItems.First().Count,
+                }).ToList();
+
+            //Assert
+            using (new AssertionScope())
+            {
+                await _orderRepository.Received(1).Save(order);
+                await _eventBus.Received(1).Publish(Arg.Is<OrderPlaced>(o => o.Code == order.Code));
+            }
+        }
+    }
+}
