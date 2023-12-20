b201d51ac75dbcf4e97e18b9de761d54e379ae88
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-12-19 21:49:32 -0300

    feat: Start StockMap

---
+++ ShopNow.Infra/Stock/Mappers/StockMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopNow.Domain.Stock.Entities;
+
+namespace ShopNow.Infra.Stock.Mappers
+{
+    public class StockMap : IEntityTypeConfiguration<StockEntry>
+    {
+        public void Configure(EntityTypeBuilder<StockEntry> builder)
+        {
+            builder
+                .ToTable("tb_stock_entry");
+            builder
+                .HasKey(x => x.Id);
+            builder
+                .Property(x => x.Code)
+                .HasColumnName("code");
+        }
+    }
+}
