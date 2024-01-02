f41c28d91cdf2fdfd4ea3be5f965e95fdbc6ad7b
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-01-02 07:46:07 -0300

    feat: Install fluent validation

---
+++ ShopNow/Validators/PlaceOrderInputValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ShopNow.Dtos;
+
+namespace ShopNow.Validators
+{
+    public class PlaceOrderInputValidator : AbstractValidator<PlaceOrderInput>
+    {
+        public PlaceOrderInputValidator()
+        {
+            
+        }
+    }
+}
